cc43194479047c2963f5142ca6466c7020014982
mahan <mdpe84.ir@gmail.com>
2022-01-31 01:09:09 +0330

    first stable version

--- js/main.js
+++ js/main.js
@@ -1,54 +1,108 @@
-
 (function ($) {
     "use strict";
 
-    
+
     /*==================================================================
     [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var subject = $('.validate-input input[name="subject"]');
-    var message = $('.validate-input textarea[name="message"]');
 
+    var links = $('.validate-input textarea[name="links"]');
+    var outputs = $('.validate-input textarea[name="outputs"]');
+    const siteUrl = 'https://www.digitalbam.ir/';
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
+        var loading = false;
 
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
+        try {
+
+
+            if ($(links).val().trim() == '') {
+                showValidate(links);
+                check = false;
+
+            }
+
+            if (check) {
+
+                const value = $(links).val();
+                let linkList = value.split(/\r?\n/);
+                document.getElementById('outputs').value = "";
+                document.getElementById("loading").removeAttribute("hidden");
+                document.getElementById("unloaidng").setAttribute("hidden", "hidden");
+                document.getElementById("loadingIcon").removeAttribute("hidden");
+                document.getElementById("unloadingIcon").setAttribute("hidden", "hidden");
+                loading = true;
+
+
+                linksGenerator(linkList).then(r => {
+                    loading = false;
+                    document.getElementById("unloaidng").removeAttribute("hidden");
+                    document.getElementById("loading").setAttribute("hidden", "hidden");
+                    document.getElementById("unloadingIcon").removeAttribute("hidden");
+                    document.getElementById("loadingIcon").setAttribute("hidden", "hidden");
+                    var x = document.getElementById("snackbar");
+                    x.className = "show";
+                    setTimeout(function () {
+                        x.className = x.className.replace("show", "");
+                    }, 3000);
+                }) ;
+
+
+
+
+            }
+
+
+        } catch (e) {
+            console.log(e);
+            return false;
+
         }
 
-        if($(subject).val().trim() == ''){
-            showValidate(subject);
-            check=false;
-        }
-
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
-        }
-
-        return check;
+        return false;
     });
 
 
-    $('.validate-form .input1').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
+    $('.validate-form .input1').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+
+
+        });
     });
+
+    async function linksGenerator(linkList){
+        for (let i = 0; i < linkList.length; i++) {
+            if (linkList[i].trim() != '') {
+                linkList[i] = linkList[i].trim();
+                let link = linkList[i];
+                if (link.indexOf('http') === 0) {
+
+
+                    await  $.post("https://www.digitalbam.ir/Home/DetectSearchPrase", {Phrase: encodeURIComponent(link)}, function (data) {
+
+
+                        if (data === "downloadLink")
+                            $.post("https://www.digitalbam.ir/DirectLinkDownloader/Download", {downloadUri: encodeURIComponent(link)}, function (data) {
+                                // window.location.href = data.fileUrl;
+                                document.getElementById('outputs').value += data.fileUrl + '\n\n';
+
+                            });
+                        // }
+                    });
+
+
+                }
+
+            }
+        }
+    }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
 
         $(thisAlert).addClass('alert-validate');
+        $(thisAlert).focus();
     }
 
     function hideValidate(input) {
@@ -56,7 +110,6 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
+
 
 })(jQuery);
